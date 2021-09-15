9888d98315cfe6f9bc61aff3dded34a52b098a1c
shahid <shahid@braincrop.net>
2021-09-15 11:32:08 +0500

    canvas image minuplation learning

---
+++ js/main.js
@@ -0,0 +1,104 @@
+(()=>{
+    const image = new Image();
+    image.src = "./img/sonic.png";
+    image.addEventListener("load",()=>{
+        const canvas = document.getElementById("image_container");
+        const ctx = canvas.getContext('2d');
+        canvas.width = image.width;
+        canvas.height = image.height;
+        ctx.drawImage(image,0,0);
+        const pixels = ctx.getImageData(0,0,canvas.width,canvas.height);
+        
+        let particales = [];
+        const numberOfParticales = 5000;
+        let mappedImage = [];
+        for(let y=0;y<canvas.height;y++){
+            let row = [];
+            for(let x = 0; x< canvas.width;x++){
+                const red = pixels.data[(y * 4 * pixels.width)+ (x * 4)];
+                const green = pixels.data[(y * 4 * pixels.width)+ (x * 4 + 1)];
+                const blue = pixels.data[(y * 4 * pixels.width)+ (x * 4 + 2)];
+                const brightness = calculateBirghtness(red,green,blue);
+                const cell = [
+                    cellBrightness = brightness
+                ];
+                row.push(cell);
+            }
+            mappedImage.push(row);
+        }
+        // console.log(mappedImage);
+        function calculateBirghtness(red,green,blue){
+            return Math.sqrt(
+                (red * red) * 0.299 +
+                (green  * green) * 0.587 +
+                (blue * blue) * 0.114
+            )/100;
+        }
+        class Particle {
+            constructor(){
+                this.x = Math.random() * canvas.width;
+                this.y = 0;
+                this.speed = 0;
+                this.velocity = Math.random() * 0.5;
+                this.size = Math.random() * 1.5 + 1;
+                this.position1 = Math.floor(this.y);
+                this.position2 = Math.floor(this.x);
+            }
+            update(){
+                this.position1 = Math.floor(this.y);
+                this.position2 = Math.floor(this.x);
+                this.speed = mappedImage[this.position1][this.position2][0];
+                let movement = (2.5 - this.speed) + this.velocity;
+                this.y += movement;
+                if(this.y >= canvas.height){
+                    this.y=0;
+                    this.x = Math.random() * canvas.width;
+                }
+            }
+            draw(){
+                ctx.beginPath();
+                ctx.fillStyle='white';
+                ctx.arc(this.x,this.y,this.size,0,Math.PI * 2);
+                ctx.fill();
+            }
+        }
+
+        function init(){
+            for(let i=0;i<numberOfParticales;i++){
+                particales.push(new Particle());
+            }
+        }
+        init();
+
+        function animate(){
+            // ctx.drawImage(image,0,0);
+            ctx.globalAlpha = 0.05;
+            ctx.fillStyle = 'rgb(0,0,0)';
+            ctx.fillRect(0,0,canvas.width,canvas.height);
+            ctx.globalAlpha = 0.2;
+            for(let i = 0; i<particales.length;i++){
+                particales[i].update();
+                ctx.globalAlpha = particales[i].speed * 0.05;
+                particales[i].draw();
+            }
+            requestAnimationFrame(animate);
+        }
+        animate();
+        /**
+        grayScale(ctx);
+        function grayScale(context){
+            const imageData = context.getImageData(0,0, canvas.width,canvas.height);
+            const scanData = imageData.data;
+            for(let i=0;i<scanData.length;i+=4){
+                let total = scanData[i]+ scanData[i+1] + scanData[i+2];
+                let average = total/3;
+                scanData[i] = average;
+                scanData[i+1] = average;
+                scanData[i+2] = average;
+            }
+            imageData.data = scanData;
+            context.putImageData(imageData,0,0);
+        }
+         */
+    });
+})();
